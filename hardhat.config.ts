243a0248d0ee5f83aea42e7654dc005b065bed6c
Sergey <dev.zaycev@gmail.com>
2022-10-26 22:23:57 +0300

    implements test structure

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
