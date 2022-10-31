36b1f18165e3408c1d6280b22c6cb23466849e64
Sergey <dev.zaycev@gmail.com>
2022-10-31 14:02:13 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import Receiver from "./receiver/Receiver";
-import ReceiverConfig from "./receiver/ReceiverConfig";
-
-export { Receiver, ReceiverConfig };
-export default Receiver;
+export * from "./database/Provider";
+export * from "./database/SqliteProvider";
+export * from "./receiver/Receiver";
+export * from "./receiver/ReceiverConfig";
